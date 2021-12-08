44080957269b5a6a263a99ce3ca64ce336eb5b18
Nigam-Niti <58980784+Nigam-Niti@users.noreply.github.com>
2021-12-08 19:35:41 +0530

    Create memory_config.py

---
+++ memory_config.py
@@ -0,0 +1,19 @@
+# memory footprint support libraries/code
+!ln -sf /opt/bin/nvidia-smi /usr/bin/nvidia-smi
+!pip install gputil
+!pip install psutil
+!pip install humanize
+
+import psutil
+import humanize
+import os
+import GPUtil as GPU
+
+GPUs = GPU.getGPUs()
+# XXX: only one GPU on Colab and isn’t guaranteed
+gpu = GPUs[0]
+def printm():
+    process = psutil.Process(os.getpid())
+    print("Gen RAM Free: " + humanize.naturalsize(psutil.virtual_memory().available), " |     Proc size: " + humanize.naturalsize(process.memory_info().rss))
+    print("GPU RAM Free: {0:.0f}MB | Used: {1:.0f}MB | Util {2:3.0f}% | Total     {3:.0f}MB".format(gpu.memoryFree, gpu.memoryUsed, gpu.memoryUtil*100, gpu.memoryTotal))
+printm()
